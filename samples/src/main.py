b2947cc4f540fb309d2fcc94e2e76947dfac0aba
take <audi.honda.love.car@gmail.com>
2019-09-08 17:37:40 +0900

    Catクラスに__str__メソッドなしでの実行サンプル用にmian.pyを変更

--- samples/src/main.py
+++ samples/src/main.py
@@ -5,9 +5,5 @@ from datetime import date
 
 from cat import Cat
 
-# jiji = Cat("ジジ", 13, "オス", "ニシンパイ", "黒", "白", "短毛種")
 jiji = Cat("ジジ", date(2017, 5, 10), "オス", "", "ニシンパイ", "黒", "白", "短毛種")
-print(jiji.name)
-# print(jiji.sex)
-print(jiji.birth_day)
-print(jiji.age)
+print(jiji)
